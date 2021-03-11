cce06ee9f124c83b51d09ec3a7d4a3074b566cb1
Documenter.jl <documenter@juliadocs.github.io>
2021-03-11 00:21:29 +0000

    build based on 8f39fb1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StatsDiscretizations","category":"page"},{"location":"#StatsDiscretizations","page":"Home","title":"StatsDiscretizations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StatsDiscretizations]","category":"page"}]
+}
